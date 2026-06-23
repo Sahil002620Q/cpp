dc1ec87cef595ed8362925a09819815153f1ec13
Sahil002620Q <smax06388@gmail.com>
2026-06-23 16:54:22 +0530

    break

---
+++ break.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main () {
+    for(int i = 1;i <= 10;++i){
+        if(i == 4){
+            break;
+        }
+            cout << i << endl ;
+    }
+    
+
+    for(int i = 1;i <= 10;++i){
+        if(i == 4){
+            break;
+        }
+            cout << i << endl ;
+    }
+}
+
+    
+    
