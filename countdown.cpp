10d55e8fe8523479cf6f80e572f8b65a8ecf2afa
Sahil002620Q <smax06388@gmail.com>
2026-06-23 16:13:13 +0530

    bomb code

--- countdown.cpp
+++ countdown.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
+#include <cstdlib>
 using namespace std;
 int main () {
     for(int i = 10;i > 0;i--){
         cout << i << endl;
-        
+        this_thread::sleep_for(chrono::seconds(1));
+        system("cls");
     }
+    system("cls");
     cout << "boom!";
 }
