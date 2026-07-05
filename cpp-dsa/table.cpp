d0e5a55a31752a978b67729968005e9c08762cef
Sahil002620Q <smax06388@gmail.com>
2026-07-05 13:59:23 +0530

    table writer and grade checker code in cpp  --commiting after 10-12 days

---
+++ cpp-dsa/table.cpp
@@ -0,0 +1,43 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int x ;
+//     cout << "enter a number to get it's table :"; cin >> x;
+//     int lim;
+//     cout << "enter a number upto which you want to get table :"; cin >> lim;
+//     int i = 1;
+//     while(i <= lim){
+//         cout << x <<" x " << i << " = " << x*i << endl;
+//         i++;
+//     }
+
+
+//     return 0;
+// }
+
+
+
+// #include <iostream>
+// using namespace std;
+// int main () {
+//   int table,range;
+//   cout << "enter number : " ; cin >> table ;  
+//   cout << "enter range : " ; cin >> range ;
+//   for(int i = 1;i <= range;  i++){
+//     cout << i*table << endl;
+//   }
+// }
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+		for(int i=1;i<=10;i++){
+      cout << n << " x " << i << " = " << i*n << endl ;
+    // Print multiplication table
+   
+    }
+}
