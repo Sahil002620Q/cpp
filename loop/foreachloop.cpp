180fb4de91b815fed5372647478f7baa19b74bf6
Sahil002620Q <smax06388@gmail.com>
2026-07-09 07:02:42 +0530

    for loop and example

---
+++ loop/foreachloop.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main () {
+
+    // int array[5] = {1,2,3,4,5};
+    // for(int i :array){
+    //     cout << i << endl ;
+    // }
+
+    // string name = "sahil";
+    // for(char j : name){
+    //     cout << j << endl ;
+    // }
+
+    int i;
+    for(i = 1 ; i <= 512 ; i = i*2)
+    {
+        cout << i << endl;
+    }
+
+}
