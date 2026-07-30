af9b224d22cf97b4500fd18d032ebf62422bcd35
Sahil002620Q <smax06388@gmail.com>
2026-07-30 22:31:50 +0530

    template in cpp --used to make templates and later we can use that template and use any data type with that template , increasing code reusability

--- OOP/template.cpp
+++ OOP/template.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 using namespace std;
 
-class x {
-
-};
-
+template <typename T>
+T add(T a,T b){
+    return a + b;
+}
+   
 int main() {
-
+    cout << add<int>(2,3) << endl ;
+    cout << add<float>(2.5,3) << endl ;
+    cout << add<double>(24,3.4) << endl ;
 }
