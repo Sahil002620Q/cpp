27e213c7d0a9ba912715e6297ba7c76fd76cd31c
Sahil002620Q <smax06388@gmail.com>
2026-07-30 22:37:21 +0530

    date and time in cpp -use ctime lib to access the functions related to time , datatype used to store time is time_t , use time() and pass pointer arg to it , it ask for pointer and under the hood it takes that pointer and give it time , it give time is seconds from epoch since 1 jan 1970

--- OOP/DateTime.cpp
+++ OOP/DateTime.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
 
     time_t now = time(nullptr); //time_t is kindaa datatype , now is variable , time() is used to fetch current time
-    cout << now << endl;
+    cout << now << endl; //using nullptr we are saying that we are not giving you any ptr loc hence it will return that value there and that returned value will be assigned to now variable
     cout <<  "second since epoch from 1 jan 1970 : " << now << endl ;
     cout << "current time : " << ctime(&now);
 
