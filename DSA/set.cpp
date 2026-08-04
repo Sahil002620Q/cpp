30ed2536e9cfcae43acf644d0384dd0376d5edcd
Sahil002620Q <smax06388@gmail.com>
2026-08-04 20:32:27 +0530

    set in cpp , simmilar to set in math , keep unique items , sort data in ascending order , no indexing

--- DSA/set.cpp
+++ DSA/set.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main() {
     set<int> num = {1,4,2,5,3,2,6,5,8,6,9,7,0,8,5,6,8,2,4}; //unique element + arranged in order , cannot access via index
+    
     for (int i : num)
     {
         cout << i << " " ;
@@ -13,6 +14,7 @@ int main() {
 
     set<string> car = {"BMW","Volvo","Verna"};
     car.erase("Volvo");
+
     for (string i : car)
     {
         cout << i << " " ;
