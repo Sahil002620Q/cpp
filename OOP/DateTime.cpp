42c2ae9446bc0fa538f660194d8006ab73ffb206
Sahil002620Q <smax06388@gmail.com>
2026-08-14 23:51:14 +0530

    get current time in epoch second or format it to human readable form using ctime() function which takes arg in second and convert it to human readable form

--- OOP/DateTime.cpp
+++ OOP/DateTime.cpp
@@ -9,5 +9,7 @@ int main() {
     cout << now << endl; //using nullptr we are saying that we are not giving you any ptr loc hence it will return that value there and that returned value will be assigned to now variable
     cout <<  "second since epoch from 1 jan 1970 : " << now << endl ;
     cout << "current time : " << ctime(&now);
+    cout << "Unix epoch was starts at 1 jan 1970 00:00:00 UTC " ;
+    cout << "so time stored as second sinc unix epoch";
 
 }
